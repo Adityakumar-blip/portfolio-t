d30ae6f03d862ebb0c916f78339f00191958d3c1
unsolved-solutions <146564388+unsolved-solutions@users.noreply.github.com>
2024-05-15 00:42:04 +0530

    code changed

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -129,7 +129,7 @@ const Testimonial = ({ props }) => {
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {item?.title}
               </h5>
-              <p class="font-normal text-gray-700 dark:text-gray-400">
+              <p class="font-normal font-mono text-gray-700 dark:text-gray-400">
                 {item?.desc}
               </p>
             </a>
