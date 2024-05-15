09ca83473f88209cac13d64f57cfb41d6f79d099
Aditya <aditya@sevensquaretech.com>
2024-05-15 09:47:23 +0530

    router issue fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/tannu-singh");
+  }, []);
   return <Component {...pageProps} />;
 }
